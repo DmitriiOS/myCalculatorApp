a5f7b58dd45e686637d7394d0cab703165bd41f2
Dmitrii <dsorlov86@gmail.com>
2020-09-18 11:14:17 +0300

    Initial Commit

---
+++ myCalc/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myCalc
+//
+//  Created by 1 on 18.09.2020.
+//  Copyright © 2020 DmitriOS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
